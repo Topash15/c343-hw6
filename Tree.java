6126492233c850725cc2d82997079dece4a8db09
Alexander Topash <Alextopash@gmail.com>
2026-03-08 10:45:38 -0400

    list_subtree and sum_of_balances functionality

--- Tree.java
+++ Tree.java
@@ -22,6 +22,13 @@ public class Tree
         // your code goes here:
         // display the contents of all nodes in this subtree t
         // in in-order
+        if(t == null){
+            return;
+        }
+
+        list_subtree(t.left);
+        System.out.println(t.account_number + " " + t.name + " " + t.balance); //1 Henry Smith 99.23
+        list_subtree(t.right);
     }
     
     public double sum_of_balances()
@@ -33,6 +40,14 @@ public class Tree
     {
 	// your code goes here:
         // return the total balances all accounts in this subtree t
+        if(t == null){
+            return 0;
+        }
+        double total = t.balance;
+
+        total += sum_of_balances_subtree(t.left);
+        total += sum_of_balances_subtree(t.right);
+        return total;
     }
     
     public int height()
@@ -44,6 +59,15 @@ public class Tree
     {
 	// your code goes here:
         // return the height of this subtree t
+        if(t == null){
+            return -1;
+        }
+        int height = 0;
+
+        if(t.left != null || t.right != null){
+
+        }
+        return height;
     }
 }
 
