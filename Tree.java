150b783af447ead4277b8b1cece37b145bc8c941
Alexander Topash <Alextopash@gmail.com>
2026-03-15 08:04:53 -0400

    final

--- Tree.java
+++ Tree.java
@@ -1,46 +1,39 @@
-public class Tree
-{
+public class Tree {
     private TreeNode root;
 
-    public Tree()
-    {
+    public Tree() {
         root = null;
     }
-    
-    public void resetRoot(TreeNode n)
-    {
+
+    public void resetRoot(TreeNode n) {
         root = n;
     }
-    
-    public void list_all_nodes()
-    {
+
+    public void list_all_nodes() {
         list_subtree(root);
     }
-	   
-    private void list_subtree(TreeNode t)
-    {
+
+    private void list_subtree(TreeNode t) {
         // your code goes here:
         // display the contents of all nodes in this subtree t
         // in in-order
-        if(t == null){
+        if (t == null) {
             return;
         }
 
         list_subtree(t.left);
-        System.out.println(t.account_number + " " + t.name + " " + t.balance); //1 Henry Smith 99.23
+        System.out.println(t.account_number + " " + t.name + " " + t.balance); // 1 Henry Smith 99.23
         list_subtree(t.right);
     }
-    
-    public double sum_of_balances()
-    {
-	return sum_of_balances_subtree(root);
+
+    public double sum_of_balances() {
+        return sum_of_balances_subtree(root);
     }
 
-    private double sum_of_balances_subtree(TreeNode t)
-    {
-	// your code goes here:
+    private double sum_of_balances_subtree(TreeNode t) {
+        // your code goes here:
         // return the total balances all accounts in this subtree t
-        if(t == null){
+        if (t == null) {
             return 0;
         }
         double total = t.balance;
@@ -49,26 +42,41 @@ public class Tree
         total += sum_of_balances_subtree(t.right);
         return total;
     }
-    
-    public int height()
-    {
-	return height_subtree(root);
+
+    public int height() {
+        return height_subtree(root);
     }
 
-    private int height_subtree(TreeNode t)
-    {
-	// your code goes here:
-        // return the height of this subtree t
-        if(t == null){
+    private int height_subtree(TreeNode t) {
+
+        // return -1 if tree is empty
+        if (t == null) {
             return -1;
         }
+
+        // if tree is not empty, there is at least one node. Height is at least 0
         int height = 0;
 
-        if(t.left != null || t.right != null){
+        // if the root has any children, height is 1
+        if (t.left != null || t.right != null) {
+            height = 1;
+        }
 
+        // check the height of the left and right nodes
+        int leftHeight = height_subtree(t.left);
+        int rightHeight = height_subtree(t.right);
+
+        // If the nodes have a height, add the total height to them
+        // Set height to the larger of the two
+        if (leftHeight > 0 || rightHeight > 0) {
+            leftHeight += height;
+            rightHeight += height;
+            if (leftHeight > rightHeight) {
+                height = leftHeight;
+            } else {
+                height = rightHeight;
+            }
         }
         return height;
     }
 }
-
-
